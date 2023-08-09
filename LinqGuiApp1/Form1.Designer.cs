25514a9a87004d0cf6e1fd5b1caa47dad4e7f2a5
yes25 <programmer.yes25@gmail.com>
2023-08-09 12:15:05 +0900

    데이터 불러오기 완성!

--- LinqGuiApp1/Form1.Designer.cs
+++ LinqGuiApp1/Form1.Designer.cs
@@ -28,31 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.foodBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.모두보기 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foodBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.foodBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
             this.dataGridView1.Size = new System.Drawing.Size(269, 312);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // nameDataGridViewTextBoxColumn
             // 
-            this.button1.Location = new System.Drawing.Point(297, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(149, 100);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "모두 보기";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            this.priceDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // foodBindingSource
+            // 
+            this.foodBindingSource.DataSource = typeof(LinqGuiApp1.Food);
+            // 
+            // 모두보기
+            // 
+            this.모두보기.Location = new System.Drawing.Point(297, 12);
+            this.모두보기.Name = "모두보기";
+            this.모두보기.Size = new System.Drawing.Size(149, 100);
+            this.모두보기.TabIndex = 1;
+            this.모두보기.Text = "button1";
+            this.모두보기.UseVisualStyleBackColor = true;
+            this.모두보기.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -60,7 +91,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(149, 100);
             this.button2.TabIndex = 2;
-            this.button2.Text = "1500원 이상 가격 비싼순서로 보기";
+            this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -70,8 +101,9 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(149, 100);
             this.button3.TabIndex = 3;
-            this.button3.Text = "이름 내림차순 보기";
+            this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -80,11 +112,12 @@
             this.ClientSize = new System.Drawing.Size(458, 335);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.모두보기);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
-            this.Text = "3112 조예서";
+            this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.foodBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -92,9 +125,12 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button 모두보기;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource foodBindingSource;
     }
 }
 
--- LinqGuiApp1/Form1.cs
+++ LinqGuiApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,16 @@ namespace LinqGuiApp1
 {
     public partial class Form1 : Form
     {
+        static  private List<Food> foodList = new List<Food>() {
+            new Food(){ Name = "떡볶이", Price=3500},
+            new Food(){ Name = "탕후루", Price=3000},
+            new Food(){ Name = "햄버거", Price=9000},
+            new Food(){ Name = "짜장면", Price=8000},
+            new Food(){ Name = "토스트", Price=2500},
+            new Food(){ Name = "껌", Price=1000},
+            new Food(){ Name = "빼빼로", Price=1700},
+        };
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +30,23 @@ namespace LinqGuiApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            foodBindingSource.DataSource = foodList;
+        }
 
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foodBindingSource.DataSource = from food in foodList
+                                           orderby food.Price descending
+                                           select food;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            foodBindingSource.DataSource = from food in foodList
+                                           where food.Price >= 1500
+                                           orderby food.Price descending
+                                           select food;
         }
     }
 }
